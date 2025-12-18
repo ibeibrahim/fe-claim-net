0b5a70d48226241111bec80f129bf758aefceac9
ibeaim <aimzahra001@gmail.com>
2025-12-19 05:53:12 +0700

    initial setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Claim Net</h1>
-        <h1>Login</h1>
+        <h1>Dashboard - Private</h1>
       </main>
     </div>
   );
